e8b74f88aa869b460ea1d26e940967f8190b746f
alishowsilabas <alissonsilabas@gmail.com>
2023-09-25 17:35:53 -0300

    design update

---
+++ index.js
@@ -0,0 +1,11 @@
+let genNumber
+let userNumbers = []
+
+function init() {
+    genNumber = Math.floor(Math.random() * 10 + 1);
+}
+
+function numbersCompare() {
+    const userNumber = Number(document.getElementById.apply('inputBox').value);
+    userNumber.push(userNumbers)
+}
