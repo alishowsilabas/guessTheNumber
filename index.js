343b32f59a4f831187fc1174ee257dc3d56aad5d
alishowsilabas <alissonsilabas@gmail.com>
2023-09-26 15:54:23 -0300

    ready layout

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 let genNumber
 let userNumbers = []
 
-function init() {
-    genNumber = Math.floor(Math.random() * 10 + 1);
+function initRandom() {
+    genNumber = Math.floor(Math.random() * 100 + 1);
 }
 
 function numbersCompare() {
-    const userNumber = Number(document.getElementById.apply('inputBox').value);
-    userNumber.push(userNumbers)
+    const userNumber = Number(document.getElementById('inputBox').value);
+    userNumbers.push(' ' + userNumber)
+    document.getElementById('guesses').innerHTML = userNumbers
+
+    if (userNumber > genNumber) {
+        document.getElementById('resultWarning').innerHTML = 'Seu número é mais alto'
+    } else if (userNumber < genNumber){
+document.getElementById('resultWarning').innerHTML = 'Seu número é mais baixo'
+    } else { 
+
+    }
 }
+
+document.getElementById('harryAudio').volume = 0.2
