4cf19f7ff08fe593f2a1235fe3e98ecbd158d965
alishowsilabas <alissonsilabas@gmail.com>
2023-09-27 13:02:26 -0300

    lock text on lose or winning added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let genNumber
 let attemptsQtd = []
 let userNumbers = []
+let maxGuesses = 10
 
 function initRandom() {
     genNumber = Math.floor(Math.random() * 100 + 1);
-    console.log(genNumber);
+    // console.log(genNumber);
 }
 
 function numbersCompare() {
@@ -16,13 +17,25 @@ function numbersCompare() {
     attemptsQtd.push(attempts);
     document.getElementById('attempts').innerHTML = attemptsQtd.length
 
-    if (userNumber > genNumber) {
+    if(attemptsQtd.length < maxGuesses){
+        if (userNumber > genNumber) {
+        document.getElementById('textOutput').style.color = "#FFFF99";
         document.getElementById('textOutput').innerHTML = 'Seu palpite é muito alto'
-    } else if (userNumber < genNumber){
+        document.getElementById('inputBox'). value = ''
+        } else if (userNumber < genNumber) {
+        document.getElementById('textOutput').style.color = "#99CCFF";
         document.getElementById('textOutput').innerHTML = 'Seu palpite é muito baixo'
-    } else { 
-        document.getElementById('textOutput').innerHTML = 'Parabéns, você acertou!'
+        document.getElementById('inputBox'). value = ''
+        } else { 
+        document.getElementById('textOutput').style.color = "#66FF99";
+        document.getElementById('textOutput').innerHTML = 'Parabéns, você acertou! &#128513;'
+        document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
+        }
+    } else {
+        document.getElementById('textOutput').style.color = "#FF6633";
+        document.getElementById('textOutput').innerHTML = 'Você perdeu! &#128517; o Número Mágico era: ' + genNumber
+        document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
     }
 }
 
-document.getElementById('harryAudio').volume = 0.2
+document.getElementById('harryAudio').volume = 0.09
