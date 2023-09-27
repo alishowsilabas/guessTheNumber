a5df8c2644ea79cf7c61916b2d933b18ad7b7c34
alishowsilabas <alissonsilabas@gmail.com>
2023-09-27 09:15:55 -0300

    working with a lot of improvements to make

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 let genNumber
+let attemptsQtd = []
 let userNumbers = []
 
 function initRandom() {
     genNumber = Math.floor(Math.random() * 100 + 1);
+    console.log(genNumber);
 }
 
 function numbersCompare() {
     const userNumber = Number(document.getElementById('inputBox').value);
-    userNumbers.push(' ' + userNumber)
+    userNumbers.push(' ' + userNumber);
     document.getElementById('guesses').innerHTML = userNumbers
+    
+    const attempts = Number(document.getElementById('inputBox').value);
+    attemptsQtd.push(attempts);
+    document.getElementById('attempts').innerHTML = attemptsQtd.length
 
     if (userNumber > genNumber) {
-        document.getElementById('resultWarning').innerHTML = 'Seu número é mais alto'
+        document.getElementById('textOutput').innerHTML = 'Seu palpite é muito alto'
     } else if (userNumber < genNumber){
-document.getElementById('resultWarning').innerHTML = 'Seu número é mais baixo'
+        document.getElementById('textOutput').innerHTML = 'Seu palpite é muito baixo'
     } else { 
-
+        document.getElementById('textOutput').innerHTML = 'Parabéns, você acertou!'
     }
 }
 
