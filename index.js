b6fdebe2d60f96a54e87a32957ce78a5ae229da3
alishowsilabas <alissonsilabas@gmail.com>
2023-09-27 18:34:20 -0300

    atualização falha de tentar travar o numero

--- index.js
+++ index.js
@@ -3,33 +3,49 @@ let attemptsQtd = []
 let userNumbers = []
 let maxGuesses = 10
 
+function newGame() {
+    window.location.reload()
+}
+
 function initRandom() {
     genNumber = Math.floor(Math.random() * 100 + 1);
     // console.log(genNumber);
+}
+
+function validateOnEnter(event) {
+    if (event.key === "Enter") {
+        var input = document.getElementById('inputBox');
+        var value = Number(input.value);
+
+        if (isNaN(value) || value < Number(input.min) || value > Number(input.max)) {
+            alert('Por favor, insira um número entre ' + input.min + ' e ' + input.max);
+            event.preventDefault(); // Impede o envio do formulário
+        }
+    }
 }
 
 function numbersCompare() {
     const userNumber = Number(document.getElementById('inputBox').value);
     userNumbers.push(' ' + userNumber);
     document.getElementById('guesses').innerHTML = userNumbers
-    
+
     const attempts = Number(document.getElementById('inputBox').value);
     attemptsQtd.push(attempts);
     document.getElementById('attempts').innerHTML = attemptsQtd.length
 
-    if(attemptsQtd.length < maxGuesses){
+    if (attemptsQtd.length < maxGuesses) {
         if (userNumber > genNumber) {
-        document.getElementById('textOutput').style.color = "#FFFF99";
-        document.getElementById('textOutput').innerHTML = 'Seu palpite é muito alto'
-        document.getElementById('inputBox'). value = ''
+            document.getElementById('textOutput').style.color = "#FFFF99";
+            document.getElementById('textOutput').innerHTML = 'Seu palpite é muito alto'
+            document.getElementById('inputBox').value = ''
         } else if (userNumber < genNumber) {
-        document.getElementById('textOutput').style.color = "#99CCFF";
-        document.getElementById('textOutput').innerHTML = 'Seu palpite é muito baixo'
-        document.getElementById('inputBox'). value = ''
-        } else { 
-        document.getElementById('textOutput').style.color = "#66FF99";
-        document.getElementById('textOutput').innerHTML = 'Parabéns, você acertou! &#128513;'
-        document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
+            document.getElementById('textOutput').style.color = "#99CCFF";
+            document.getElementById('textOutput').innerHTML = 'Seu palpite é muito baixo'
+            document.getElementById('inputBox').value = ''
+        } else {
+            document.getElementById('textOutput').style.color = "#66FF99";
+            document.getElementById('textOutput').innerHTML = 'Parabéns, você acertou! &#128513;'
+            document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
         }
     } else {
         document.getElementById('textOutput').style.color = "#FF6633";
@@ -38,4 +54,4 @@ function numbersCompare() {
     }
 }
 
-document.getElementById('harryAudio').volume = 0.09
+document.getElementById('harryAudio').volume = 0.05
